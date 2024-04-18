7fb2074a60aa10ff6fcfb1118d421486e4be7f5e
Humberto Fraga <humbertofraga@gmail.com>
2024-04-18 18:25:31 -0300

    Inicia configuração do MaxScale

--- mariadb-replication/host03/init/init.sql
+++ mariadb-replication/host03/init/init.sql
@@ -1,3 +1,18 @@
+CREATE USER 'monitor_user'@'%' IDENTIFIED BY 'monitorsecret';
+GRANT REPLICATION CLIENT on *.* to 'monitor_user'@'%';
+
+GRANT SUPER, RELOAD on *.* to 'monitor_user'@'%';
+
+CREATE USER 'maxscale'@'%' IDENTIFIED BY 'maxscale_pw';
+GRANT SELECT ON mysql.user TO 'maxscale'@'%';
+GRANT SELECT ON mysql.db TO 'maxscale'@'%';
+GRANT SELECT ON mysql.tables_priv TO 'maxscale'@'%';
+GRANT SELECT ON mysql.columns_priv TO 'maxscale'@'%';
+GRANT SELECT ON mysql.procs_priv TO 'maxscale'@'%';
+GRANT SELECT ON mysql.proxies_priv TO 'maxscale'@'%';
+GRANT SELECT ON mysql.roles_mapping TO 'maxscale'@'%';
+GRANT SHOW DATABASES ON *.* TO 'maxscale'@'%';
+
 CHANGE MASTER TO
   MASTER_HOST='host01',
   MASTER_USER='repluser',
