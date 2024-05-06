f40b5d8a5d1e9b434c940d584716235381fea7d2
Humberto Fraga <humbertofraga@gmail.com>
2024-05-06 16:12:31 -0300

    Reconfigura YCSB

--- mariadb-replication/ycsb/entrypoint.sh
+++ mariadb-replication/ycsb/entrypoint.sh
@@ -8,10 +8,10 @@ done
 
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
-echo "Iniciando YCSB em $TIMESTAMP"
+echo "Iniciando YCSB em $HOSTNAME - $TIMESTAMP"
 
-/app/ycsb-0.17.0/bin/ycsb.sh load jdbc -P workloads/workloada -s -P db.properties -P replica.properties
+/app/ycsb-0.17.0/bin/ycsb.sh load jdbc -P workloads/workloada -s -P load.properties -P replica.properties > /results/$HOSTNAME-load-$TIMESTAMP.txt
 
-/app/ycsb-0.17.0/bin/ycsb.sh run  jdbc -P workloads/workloada -s -P db.properties -P replica.properties > /results/$HOSTNAME-$TIMESTAMP.dat
+/app/ycsb-0.17.0/bin/ycsb.sh run  jdbc -P workloads/workloada -s -P run.properties > /results/$HOSTNAME-run-$TIMESTAMP.txt
 
 exit
